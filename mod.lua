d9f0b177e6fe787a81efdc7fcb757c01c5b6b13b
Florian Thiel <flth@flth.de>
2023-11-22 21:07:27 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,65 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		    {
+		        name = "Kaleut",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "deutschland", "germany", "schweiz", "db", "waggon", "goods", "Intermodal", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	  
+		params = {
+			{
+				key = "sggmrs104fake",
+				name = _("Fake_sggmrs104_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_wagen_desc"),
+				defaultIndex = 0,
+			},	
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
+
+        local hidden = {
+			["aae_fake.mdl"] = true,
+			["db_fake.mdl"] = true,
+			["db2_fake.mdl"] = true,
+			["kombi_fake.mdl"] = true,
+			["kombi2_fake.mdl"] = true,
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/sggmrs104_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["sggmrs104fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+		
+		addModifier( "loadModel", metadataHandler )
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,57 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Container wagon Sggmrs 714/715",
+		["mod_desc"] = "From 1988 onwards, 300 six-axle container wagon units were built at WU Berlin. In 1991, another 1,250 units were manufactured, which are still used by DB AG today as Sggmrs 715.",
+
+        ["sggmrs104_menu"] = "Sggmrss 714/715 Container wagon",
+		["sggmrs104_aae_name"] = "Sggmrss 104 AAE (2012)",
+		["sggmrs104_kombi_name"] = "Sggmrss 715 Kombiwaggon (1988)",
+		["sggmrs104_kombi2_name"] = "Sggmrss 715 Kombiwaggon (1991)",
+		["sggmrs104_db_name"] = "Sggmrss 104 DB (1994)",
+		["sggmrs104_db2_name"] = "Sggmrss 104 DB (1997) PIC",
+        ["sggmrs104_desc"] = "From 1988 onwards, 300 six-axle container wagon units were built at WU Berlin. In 1991, another 1,250 units were manufactured, which are still used by DB AG today as Sggmrs 715.",
+        ["sggmrs104_desc_pic"] = "n 1997, the Sggmrs 715 were approved for high-speed freight traffic at 140 km/h by fixing the sliders in the longitudinal direction; the car was redesignated Sggmrs 714. The wagons are still used on Parcel Intercity (PIC) trains today.",
+
+		["option_fake_wagen_desc"] = "Should the fake versions of the Sggmrss 90 be available in the game?",
+		["Fake_sggmrs104_wagen"] = "Activate fake versions?",
+
+		--- Fake ---
+        ["sggmrs104_menu"] = "sggmrss90 Containertragwagen Fake",
+		["sggmrs104_aae_name_fake"] = "Sggmrss 104 AAE (2012) Fake",
+		["sggmrs104_kombi_name_fake"] = "Sggmrss 715 Kombiwaggon (1988) Fake",
+		["sggmrs104_kombi2_name_fake"] = "Sggmrss 715 Kombiwaggon (1991) Fake",
+		["sggmrs104_db_name_fake"] = "Sggmrss 104 DB (1994) Fake",
+		["sggmrs104_db2_name_fake"] = "Sggmrss 104 DB (1997) PIC Fake",
+		
+	},
+	de = {
+
+		["mod_name"] = "Containerwagen Sggmrs 714/715",
+		["mod_desc"] = "Ab dem Jahr 1988 wurde bei der WU Berlin 300 Sechseachsige Containertragwagen einheiten gebaut. 1991 wurde nochmals 1250 Einheiten gefertig, die bis heute bei der DB AG als Sggmrs 715 eingestellt sind.",
+
+        ["sggmrs104_menu"] = "Sggmrss 714/715 Containertragwagen",
+		["sggmrs104_aae_name"] = "Sggmrss 104 AAE (2012)",
+		["sggmrs104_kombi_name"] = "Sggmrss 715 Kombiwaggon (1988)",
+		["sggmrs104_kombi2_name"] = "Sggmrss 715 Kombiwaggon (1991)",
+		["sggmrs104_db_name"] = "Sggmrss 104 DB (1994)",
+		["sggmrs104_db2_name"] = "Sggmrss 104 DB (1997) PIC",
+        ["sggmrs104_desc"] = "Ab dem Jahr 1988 wurde bei der WU Berlin 300 Sechseachsige Containertragwagen einheiten gebaut. 1991 wurde nochmals 1250 Einheiten gefertig, die bis heute bei der DB AG als Sggmrs 715 eingestellt sind.",
+        ["sggmrs104_desc_pic"] = "1997 wurden die Sggmrs 715 durch die festlegung der Gleitstücke in Längstrichtung für den Hochgeschwindigkeits Güterverkehrt von 140km/h ertüchtigt, dabei wurde die Wagen in Sggmrs 714 umgezeichnet. Die Wagen werden bis heute auf den Parcel Intercity(PIC) Zügen eingesetzt.",
+
+		["option_fake_wagen_desc"] = "Sollen die Fake-Versionen des Sggmrs(s) 714/715 verfügbar sein im Spiel?",
+		["Fake_sggmrs104_wagen"] = "Fake-Versionen aktivieren?",
+		
+		--- Fake ---
+        ["sggmrs104_menu_Fake"] = "Sggmrs(s) 714/715 Containertragwagen Fake",
+		["sggmrs104_aae_name_fake"] = "Sggmrss 104 AAE (2012) Fake",
+		["sggmrs104_kombi_name_fake"] = "Sggmrss 715 Kombiwaggon (1988) Fake",
+		["sggmrs104_kombi2_name_fake"] = "Sggmrss 715 Kombiwaggon (1991) Fake",
+		["sggmrs104_db_name_fake"] = "Sggmrss 104 DB (1994) Fake",
+		["sggmrs104_db2_name_fake"] = "Sggmrss 104 DB (1997) PIC Fake",
+		
+		
+	},
+}
+end
