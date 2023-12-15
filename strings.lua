7e1f688fa852c52a96a5fd74ad3870ecdb45e367
Florian Thiel <flth@flth.de>
2023-12-15 17:52:39 +0100

    Strings angepaßt

--- strings.lua
+++ strings.lua
@@ -10,7 +10,7 @@ return {
 		["sggmrs104_kombi_name"] = "Sggmrss 715 Kombiwaggon (1988)",
 		["sggmrs104_kombi2_name"] = "Sggmrss 715 Kombiwaggon (1991)",
 		["sggmrs104_db_name"] = "Sggmrss 104 DB (1994)",
-		["sggmrs104_db2_name"] = "Sggmrss 104 DB (1997) PIC",
+		["sggmrs104_db2_name"] = "Sggmrss 104 DB PIC (1997)",
         ["sggmrs104_desc"] = "From 1988 onwards, 300 six-axle container wagon units were built at WU Berlin. In 1991, another 1,250 units were manufactured, which are still used by DB AG today as Sggmrs 715.",
         ["sggmrs104_desc_pic"] = "n 1997, the Sggmrs 715 were approved for high-speed freight traffic at 140 km/h by fixing the sliders in the longitudinal direction; the car was redesignated Sggmrs 714. The wagons are still used on Parcel Intercity (PIC) trains today.",
 
@@ -24,7 +24,7 @@ return {
 		["sggmrs104_kombi2_name_fake"] = "Sggmrss 715 Kombiwaggon (1991) Fake",
 		["sggmrs104_db_name_fake"] = "Sggmrss 104 DB (1994) Fake",
 		["sggmrs104_db2_name_fake"] = "Sggmrss 104 DB (1997) PIC Fake",
-		
+
 	},
 	de = {
 
@@ -36,13 +36,13 @@ return {
 		["sggmrs104_kombi_name"] = "Sggmrss 715 Kombiwaggon (1988)",
 		["sggmrs104_kombi2_name"] = "Sggmrss 715 Kombiwaggon (1991)",
 		["sggmrs104_db_name"] = "Sggmrss 104 DB (1994)",
-		["sggmrs104_db2_name"] = "Sggmrss 104 DB (1997) PIC",
+		["sggmrs104_db2_name"] = "Sggmrss 104 DB PIC (1997)",
         ["sggmrs104_desc"] = "Ab dem Jahr 1988 wurde bei der WU Berlin 300 Sechseachsige Containertragwagen einheiten gebaut. 1991 wurde nochmals 1250 Einheiten gefertig, die bis heute bei der DB AG als Sggmrs 715 eingestellt sind.",
         ["sggmrs104_desc_pic"] = "1997 wurden die Sggmrs 715 durch die festlegung der Gleitstücke in Längstrichtung für den Hochgeschwindigkeits Güterverkehrt von 140km/h ertüchtigt, dabei wurde die Wagen in Sggmrs 714 umgezeichnet. Die Wagen werden bis heute auf den Parcel Intercity(PIC) Zügen eingesetzt.",
 
 		["option_fake_wagen_desc"] = "Sollen die Fake-Versionen des Sggmrs(s) 714/715 verfügbar sein im Spiel?",
 		["Fake_sggmrs104_wagen"] = "Fake-Versionen aktivieren?",
-		
+
 		--- Fake ---
         ["sggmrs104_menu_Fake"] = "Sggmrs(s) 714/715 Containertragwagen Fake",
 		["sggmrs104_aae_name_fake"] = "Sggmrss 104 AAE (2012) Fake",
@@ -50,8 +50,7 @@ return {
 		["sggmrs104_kombi2_name_fake"] = "Sggmrss 715 Kombiwaggon (1991) Fake",
 		["sggmrs104_db_name_fake"] = "Sggmrss 104 DB (1994) Fake",
 		["sggmrs104_db2_name_fake"] = "Sggmrss 104 DB (1997) PIC Fake",
-		
-		
+
 	},
 }
 end
